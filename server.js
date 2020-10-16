ecce491e97fb656d15cb61ac71fae45fab8cf671
laport-n <nicolas-1301@live.fr>
2020-10-16 15:36:18 +0200

    ajout server.js

--- server.js
+++ server.js
@@ -1,33 +1,26 @@
-const { createServer } = require('http')
-const next = require('next')
 
+const { createServer } = require('https');
+const { parse } = require('url');
+const next = require('next');
 const fs = require('fs');
 
-const port = parseInt(process.env.PORT, 10) || 443
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-const httpsOptions = {
+const httpsOptions  = {
   cert: fs.readFileSync('/etc/letsencrypt/live/nicolas-laporte.dev/cert.pem'),
   ca: fs.readFileSync('/etc/letsencrypt/live/nicolas-laporte.dev/chain.pem'),
   key : fs.readFileSync('/etc/letsencrypt/live/nicolas-laporte.dev/privkey.pem')
 };
 
 app.prepare().then(() => {
-createServer(httpsOptions, (req, res) => {
-  const parsedUrl = new URL(req.url, 'http://w.w')
-  const { pathname, query } = parsedUrl
+  createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
 
-  if (pathname === '/a') {
-    app.render(req, res, '/a', query)
-  } else if (pathname === '/b') {
-    app.render(req, res, '/b', query)
-  } else {
-    handle(req, res, parsedUrl)
-  }
-}).listen(port, (err) => {
-  if (err) throw err
-  console.log(`> Ready on http://localhost:${port}`)
-})
-})
+  }).listen(3000, err => {
+    if (err) throw err;
+    console.log('> Ready on https://localhost:3000');
+  });
+});
